52e3d2037eb9816542254014ac38cec53b2c8523
I. Kvilinskiy <i.kvylynskyi@smsclub.mobi>
2020-10-19 14:43:01 +0300

    Urls fix

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 
-const SMS_API_URL = 'https://im.misha.dev.smsclub.mobi/sms';
-const VIBER_API_URL = 'https://im.misha.dev.smsclub.mobi/vibers';
+const SMS_API_URL = 'https://im.smsclub.mobi/sms';
+const VIBER_API_URL = 'https://im.smsclub.mobi/vibers';
